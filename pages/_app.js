1a04c939d81ec70abcce377860957d33a14f31fe
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-07-10 19:34:06 +0100

    remove next auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
-import { SessionProvider } from 'next-auth/react'
 import { FitbitProvider } from '../hooks/useFitbit'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <FitbitProvider>
-        <Component {...pageProps} />
-      </FitbitProvider>
-    </SessionProvider>
+    <FitbitProvider>
+      <Component {...pageProps} />
+    </FitbitProvider>
   )
 }
 
