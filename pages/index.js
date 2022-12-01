0d11599e3ebed2db4060d2e55081e54c679f8884
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2022-12-01 23:44:58 +0000

    loading animation on button

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import { getCookie } from 'cookies-next'
-import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Landing() {
+  const [loading, setLoading] = useState(false)
   return (
-    <>
-      Not signed in <br />
-      <Link
-        target='_self'
-        href={
-          'https://www.fitbit.com/oauth2/authorize?client_id=22BT48&expires_in=31536000&response_type=code&redirect_uri=http://localhost:3000/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social'
-        }>
-        Sign in to Fitbit
-      </Link>
-    </>
+    <div className='card'>
+      Heading
+      <sub onClick={() => setLoading(true)}>subheading</sub>
+      <div className={`login-button ${loading ? 'login-button--loading' : ''}`}>
+        <a
+          target='_self'
+          onClick={() => setLoading(true)}
+          href={
+            'https://www.fitbit.com/oauth2/authorize?client_id=22BT48&expires_in=31536000&response_type=code&redirect_uri=http://localhost:3000/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social'
+          }>
+          {loading ? '' : 'Log in with Fitbit'}
+        </a>
+      </div>
+    </div>
   )
 }
 
