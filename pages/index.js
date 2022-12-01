3aadbb2eeded4bf6744c65ba16dd4fef614a22e0
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2022-12-01 23:57:37 +0000

    fix url on index page

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ export default function Landing() {
         <a
           target='_self'
           onClick={() => setLoading(true)}
-          href={
-            'https://www.fitbit.com/oauth2/authorize?client_id=22BT48&expires_in=31536000&response_type=code&redirect_uri=http://localhost:3000/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social'
-          }>
+          href={`https://www.fitbit.com/oauth2/authorize?client_id=22BT48&expires_in=31536000&response_type=code&redirect_uri=${process.env.VERCEL_URL}/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social`}>
           {loading ? '' : 'Log in with Fitbit'}
         </a>
       </div>
