20a983896700ae7caa6e5e82aaa80a5eab26049d
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2022-12-02 01:39:54 +0000

    login screen styling, needs massive clean up honestly

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 import { getCookie } from 'cookies-next'
+import Image from 'next/image'
 import { useState } from 'react'
 
 export default function Landing() {
   const [loading, setLoading] = useState(false)
   return (
-    <div className='card'>
-      Heading
-      <sub onClick={() => setLoading(true)}>subheading</sub>
-      <div className={`login-button ${loading ? 'login-button--loading' : ''}`}>
-        <a
-          target='_self'
-          onClick={() => setLoading(true)}
-          href={`https://www.fitbit.com/oauth2/authorize?client_id=${process.env.FITBIT_CLIENT_ID}&expires_in=31536000&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_URL}/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social`}>
-          {loading ? '' : 'Log in with Fitbit'}
-        </a>
+    <div className='container'>
+      <div className='card'>
+        <div className='logo-image'>
+          <Image alt='logo' src='/logo.png' width={150} height={150} />
+        </div>
+        <h1>Cylch</h1>
+        <p className='subheading'>Use your Fitbit data against your lizard brain and hack yourself to healthy</p>
+        <div className={`login-button ${loading ? 'login-button--loading' : ''}`}>
+          <a
+            target='_self'
+            onClick={() => setLoading(true)}
+            href={`https://www.fitbit.com/oauth2/authorize?client_id=${process.env.FITBIT_CLIENT_ID}&expires_in=31536000&response_type=code&redirect_uri=${process.env.NEXT_PUBLIC_URL}/fitbit&scope=weight%20location%20settings%20profile%20nutrition%20activity%20sleep%20heartrate%20social`}>
+            {loading ? '' : 'Log in with Fitbit'}
+          </a>
+        </div>
       </div>
     </div>
   )
